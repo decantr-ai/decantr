f23ca00ca3637995fc7336c349676ff0676fc188
David Aimi <davidaimi@Davids-MacBook-Pro.local>
2026-03-29 20:35:01 -0400

    chore: create @decantr/ui package skeleton

---
+++ packages/ui/src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @decantr/ui - Decantr UI Framework
+ * Signal-based reactivity, atomic CSS, 100+ components
+ */
+
+export const VERSION = '0.1.0';
+
+// Core exports will be added as source files are migrated
+// from decantr-framework repository
