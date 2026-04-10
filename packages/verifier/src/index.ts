ebdff24f29445809a4dc3373b8f2780af1cf0410
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 17:02:09 -0400

    feat: trace locale-lowercased auth helpers

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5156,7 +5156,7 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
-    && isMemberAccessNamed(expression.expression, 'trim', 'replace', 'toLowerCase', 'toUpperCase')
+    && isMemberAccessNamed(expression.expression, 'trim', 'replace', 'toLowerCase', 'toUpperCase', 'toLocaleLowerCase')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
       sourceFile,
