44d12b62a4833cea66fdb24bc0aedb16faa9de43
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 15:46:31 -0400

    feat: trace sliced auth helper redirects

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5108,6 +5108,22 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
+    && isMemberAccessNamed(expression.expression, 'slice')
+    && expressionLooksLikeOpenRedirectQueryGetterFunction(
+      expression.expression.expression,
+      sourceFile,
+      namedExpressions,
+      namedPropertyAliases,
+      seenIdentifiers,
+      seenFunctions,
+    )
+  ) {
+    return true;
+  }
+
+  if (
+    isCallLikeExpression(expression)
+    && isMemberAccessExpression(expression.expression)
     && isMemberAccessNamed(expression.expression, 'bind')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
