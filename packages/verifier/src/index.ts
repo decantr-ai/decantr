b6278ddd0e9aeef5c69619694bacb35d3eebd5cf
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 16:50:51 -0400

    feat: trace replaced auth redirect helpers

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5156,6 +5156,22 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
+    && isMemberAccessNamed(expression.expression, 'trim', 'replace')
+    && expressionLooksLikeOpenRedirectQueryGetterFunction(
+      expression.expression.expression,
+      sourceFile,
+      namedExpressions,
+      namedPropertyAliases,
+      seenIdentifiers,
+      seenFunctions,
+    )
+  ) {
+    return true;
+  }
+
+  if (
+    isCallLikeExpression(expression)
+    && isMemberAccessExpression(expression.expression)
     && isMemberAccessNamed(expression.expression, 'values', 'next')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
