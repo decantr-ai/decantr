1a8cde875953425c2338b154bae519be92e05bd8
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 15:02:22 -0400

    feat: trace auth redirect at helpers

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5028,6 +5028,22 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
+    && isMemberAccessNamed(expression.expression, 'at')
+    && expressionLooksLikeOpenRedirectQueryGetterFunction(
+      expression.expression.expression,
+      sourceFile,
+      namedExpressions,
+      namedPropertyAliases,
+      seenIdentifiers,
+      seenFunctions,
+    )
+  ) {
+    return true;
+  }
+
+  if (
+    isCallLikeExpression(expression)
+    && isMemberAccessExpression(expression.expression)
     && isMemberAccessNamed(expression.expression, 'bind')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
