3d1c6a36784114606bbebfc53fc711a9dc847a55
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 16:59:29 -0400

    feat: trace uppercased auth redirect helpers

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5156,7 +5156,7 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
-    && isMemberAccessNamed(expression.expression, 'trim', 'replace', 'toLowerCase')
+    && isMemberAccessNamed(expression.expression, 'trim', 'replace', 'toLowerCase', 'toUpperCase')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
       sourceFile,
