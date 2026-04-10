6489e5df909fd75d1fb826e455cdf0e70774d3ff
David Aimi <davidaimi@davids-mbp.lan>
2026-04-10 17:15:03 -0400

    feat: trace legacy left-trimmed auth helpers

--- packages/verifier/src/index.ts
+++ packages/verifier/src/index.ts
@@ -5156,7 +5156,7 @@ function expressionLooksLikeOpenRedirectQueryGetterFunction(
   if (
     isCallLikeExpression(expression)
     && isMemberAccessExpression(expression.expression)
-    && isMemberAccessNamed(expression.expression, 'trim', 'trimStart', 'trimEnd', 'replace', 'toLowerCase', 'toUpperCase', 'toLocaleLowerCase', 'toLocaleUpperCase', 'normalize')
+    && isMemberAccessNamed(expression.expression, 'trim', 'trimStart', 'trimLeft', 'trimEnd', 'replace', 'toLowerCase', 'toUpperCase', 'toLocaleLowerCase', 'toLocaleUpperCase', 'normalize')
     && expressionLooksLikeOpenRedirectQueryGetterFunction(
       expression.expression.expression,
       sourceFile,
